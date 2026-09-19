cc55984ed32e28f9225c79f91f4128f6ba857683
Zephryn <jiaze8055@gmail.com>
2026-09-19 12:32:57 +0800

    Add name

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main() {
-    
+
     cout << "Hello World!" << endl;
     cout << "Hi Github!" << endl;
+    cout << "My name is Zephryn." << endl;
     cout << "I am learning C++." << endl;
     cout << "This is my first GitHub project." << endl;
     cout << "I am starting my Software Engineering journey." << endl;
