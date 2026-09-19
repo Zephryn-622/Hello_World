df825fa51665e5812467476dda2a6a97bc421783
Cheng Jia Ze <chengjiaze@Chengs-MacBook-Pro.local>
2026-09-19 12:18:13 +0800

    Update Hello World Programme

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World and Hi Github." << endl;
+    
+    cout << "Hello World!" << endl;
+    cout << "Hi Github!" << endl;
+    cout << "I am learning C++." << endl;
+    cout << "This is my first GitHub project." << endl;
+    cout << "I am starting my Software Engineering journey." << endl;
+
     return 0;
 }
